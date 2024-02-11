2c4bf77aee71f6f88733b4489f7e94ff738672e1
Linas <limax.workspace@gmail.com>
2024-02-11 20:24:11 +0100

    added hashing

--- src/app/api/createUser/route.ts
+++ src/app/api/createUser/route.ts
@@ -8,28 +8,16 @@ export async function POST(req: any, res: any) {
     const { name, email, password, avatarImage } = await req.json();
 
     await connect();
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
 
-    // Generate a salt and hash the password
-    bcrypt.genSalt(10, async function(err:any, salt:any) {
-      if (err) {
-        throw err;
-      }
-      bcrypt.hash(password, salt, async function(err:any, hashedPassword:any) {
-        if (err) {
-          throw err;
-        }
-
-        // Create a new user with the hashed password
-        const newUser = await User.create({
-          name: name,
-          email: email,
-          password: hashedPassword,
-          avatarImage: avatarImage,
-        });
-
-        return new NextResponse(JSON.stringify(newUser), { status: 200 });
-      });
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: hashedPassword, // Store the hashed password
+      avatarImage: avatarImage,
     });
+
+    return new NextResponse(JSON.stringify(newUser), { status: 200 });
   } catch (error) {
     console.error("Error creating user:", error);
     return new NextResponse(JSON.stringify({ error: "Internal server error" }), { status: 500 });
