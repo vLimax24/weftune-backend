23d6a8a1a085930637f55fa1d2d0bdb01f32b41e
Linas <limax.workspace@gmail.com>
2024-02-14 21:45:54 +0100

    added

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -3,58 +3,18 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const itemSchema = new Schema({
-  name: { type: String },
-  category: { type: String },
-  extendedCategory: { type: String },
-  description: { type: String },
-  imageUrl: { type: String },
-  characteristics: {
-    glutenFree: { type: Boolean },
-    vegan: { type: Boolean },
-    organic: { type: Boolean },
-    spicy: { type: Boolean },
-    halal: { type: Boolean },
-    kosher: { type: Boolean },
-    vegetarian: { type: Boolean },
-    allergens: { type: [String] },
+  name: {
+    type: String,
+    required: true
   },
-  nutrition: {
-    calories: { type: Number },
-    protein: { type: Number },
-    carbohydrates: { type: Number },
-    fat: { type: Number },
-    fiber: { type: Number },
-    minerals: {
-        iron: { type: Number },
-        calcium: { type: Number },
-        magnesium: { type: Number },
-        phosphorus: { type: Number },
-        potassium: { type: Number },
-        sodium: { type: Number },
-        zinc: { type: Number },
-    },
-    vitamins: {
-        A: { type: Number },
-        B: { type: Number },
-        C: { type: Number },
-        D: { type: Number },
-        E: { type: Number },
-        K: { type: Number },
-        B1: { type: Number },
-        B2: { type: Number },
-        B3: { type: Number },
-        B5: { type: Number },
-        B6: { type: Number },
-        B7: { type: Number },
-        B9: { type: Number },
-        B12: { type: Number },
-    }
+  category: {
+    type: String,
+    required: true
   },
-  storage: {
-    temperature: { type: String },
-    humidity: { type: String },
-    shelfLife: { type: String },
-  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
 });
 
 const Item = mongoose.models.Item || mongoose.model('Item', itemSchema);
