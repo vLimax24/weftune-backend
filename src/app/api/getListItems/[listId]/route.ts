e537582c0a573f5d7ada8041f7e14cfa3c18deda
Linas <limax.workspace@gmail.com>
2024-02-15 21:51:26 +0100

    added

---
+++ src/app/api/getListItems/[listId]/route.ts
@@ -0,0 +1,29 @@
+import connect from "../../../../../db";
+import List from "../../../models/list";
+import { NextResponse } from 'next/server';
+
+export const GET = async (
+    request: Request,
+    { params }: { params: { listId: string } }
+  ) => {
+    const { listId } = params;
+
+    try {
+        // Connect to the database
+        await connect();
+    
+        // Find the list by its ID
+        const list = await List.findById(listId);
+        if (!list) {
+          return new NextResponse(JSON.stringify({ error: "List not found" }), { status: 404 });
+        }
+    
+        // Remove the item from the list
+        const listItems = list.items
+    
+        return new NextResponse(JSON.stringify(listItems), { status: 200 });
+      } catch (err) {
+        console.error("Error removing item from list:", err);
+        return new NextResponse(JSON.stringify({ error: "Internal server error" }), { status: 500 });
+      }
+  }
